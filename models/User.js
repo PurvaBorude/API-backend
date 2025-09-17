3c4a584ec4be90a0f110296d5a62271c0d2fa825
Purva Borude <borudepurva@gmail.com>
2025-09-17 09:55:48 +0530

    responsiveness added

--- models/User.js
+++ models/User.js
@@ -1,14 +1,3 @@
-/*const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  username: { type: String, unique: true }, // 👈 new
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('User', userSchema);*/
-
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
